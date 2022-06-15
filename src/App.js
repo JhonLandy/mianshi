34f689fcea02a6522c596bd69c3e031636a77fc4
huaweiserver <709364178@qq.com>
2022-06-15 21:44:58 +0800

    fix:规范代码

--- src/App.js
+++ src/App.js
@@ -1,163 +1,10 @@
 import './App.css';
-import {useState, memo, Fragment, useMemo, useCallback} from "react"
+import {useState, useMemo } from "react"
 import { length } from "ramda"
+import { dp, getPrint, getCombinations, calc } from "./utils"
 import Drawer from "./components/Drawer";
-let GLOBAL_MIN_DIS = Number.MAX_VALUE
-let GLOBAL_RUN = false
-function getDistance(start, end) {
-    const [x1, y1] = start
-    const [x2, y2] = end
-    // 计算距离
-    const l = Math.abs(x1 - x2)
-    const k = Math.abs(y1 - y2)
-    return Math.sqrt(l * l, k * k)
-}
-function calc(positions, path) {
-    const _path = [...path]
-    const len = positions.length
-    let pre = _path.shift()
-    let next = _path.shift()
-    const result = []
-    let distance = 0
-    while (true) {
-        const preP= positions[pre]
-        const nextP = positions[next]
-        distance += getDistance(preP, nextP)
-        result.push([...preP, ...nextP])
-        pre = next
-        next = _path.shift()
 
-        if (next === void 0) {// 到达最后一个
-            const start = path[0]
-            const end = path[len - 1]
-            distance += getDistance(positions[start], positions[end])
-            result.push([...positions[start], ...positions[end]])
-            break
-        }
-    }
-    return [result, distance]
-}
-function getCombinations(n) {
-    let combinations = 1
-    for(let i = 1;i <= n;i++) {
-        combinations *= i
-    }
-    return combinations
-}
-function dp(node, callback) {
-    const mark = {} // 回溯标记
-    const path = []
 
-    const nLen = length(node)
-    const sum = getCombinations(nLen)
-    let isPause = false
-    let isReStart = false
-    let isStop = true
-    let result = []
-    let num = 0
-    function find(pIndex = 0) {
-        if (isStop) {
-            isPause = false
-            isReStart = false
-            num = 0
-            result = []
-            return
-        }
-        const pLen = length(path)
-        let start = 0
-        if (isReStart) {
-            isPause = false
-            if (pIndex === pLen - 1) {
-                start = path[pIndex] + 1
-                path.pop()
-                isReStart = false
-            } else {
-                start = path[pIndex]
-            }
-        } else {
-            if (pLen === nLen) {
-                result.push([...path])
-                num++
-                if (num === sum) {
-                    callback(result)
-                } else {
-                    if (length(result) >= 100) {
-                        callback(result)
-                        splitTask(() => {
-                            find()
-                        })
-                        result = []
-                        isPause = true
-                        isReStart = true
-                    }
-                }
-                return
-            }
-        }
-        for (let i = start; i < nLen;i++) {
-            if (isPause || isStop) break;
-            if (!mark[i]) {
-                mark[i] = true
-                if (!isReStart) {
-                    path.push(i)
-                }
-                find(pIndex + 1)
-                mark[i] = null
-                if (!isReStart) {
-                    path.pop()
-                };
-            }
-        }
-    }
-    return {
-        run: function () {
-            isStop = false
-            find()
-        },
-        stop: function () {
-            isStop = true
-            isPause = false
-            isReStart = false
-        }
-    }
-}
-
-function getPrint(callback) {
-    let paths = []
-    let isPaint = false
-
-    function print(paths, callback) {
-        const path = paths.shift()
-        if (!path) {
-            isPaint = false
-            return
-        }
-        requestAnimationFrame(() => {
-            if (!isPaint) {
-                return
-            }
-            callback(path)
-            print(paths, callback)
-        })
-    }
-
-    return {
-        print: () => {
-            if (!isPaint) {
-                isPaint = true
-                print(paths, callback)
-            }
-        },
-        push: newPaths => {
-            paths.push(...newPaths)
-        },
-        stopPrint: () => {
-            isPaint = false
-            paths = []
-        }
-    }
-}
-const splitTask = window.requestIdleCallback ?  requestIdleCallback : window.setTimeout
 function App() {
     const initialState = {
         circles: [],
@@ -213,7 +60,6 @@ function App() {
         } else {
             setState({ ...state, isRun: false })
             dpTer.stop()
-            printer.stopPrint()
         }
     }
     const onDraw = (e) => {
---
+++ src/utils/index.js
@@ -0,0 +1,164 @@
+import {length} from "ramda";
+
+let GLOBAL_RUN = false
+
+const splitTask = window.requestIdleCallback ?  requestIdleCallback : window.setTimeout
+
+function start() {
+    GLOBAL_RUN = true
+}
+function stop() {
+    GLOBAL_RUN = false
+}
+function getDistance(start, end) {
+    const [x1, y1] = start
+    const [x2, y2] = end
+    // 计算距离
+    const l = Math.abs(x1 - x2)
+    const k = Math.abs(y1 - y2)
+    return Math.sqrt(l * l, k * k)
+}
+function calc(positions, path) {
+    const _path = [...path]
+    const len = positions.length
+    let pre = _path.shift()
+    let next = _path.shift()
+    const result = []
+    let distance = 0
+    while (true) {
+        const preP= positions[pre]
+        const nextP = positions[next]
+        distance += getDistance(preP, nextP)
+        result.push([...preP, ...nextP])
+        pre = next
+        next = _path.shift()
+
+        if (next === void 0) {// 到达最后一个
+            const start = path[0]
+            const end = path[len - 1]
+            distance += getDistance(positions[start], positions[end])
+            result.push([...positions[start], ...positions[end]])
+            break
+        }
+    }
+    return [result, distance]
+}
+function getCombinations(n) {
+    let combinations = 1
+    for(let i = 1;i <= n;i++) {
+        combinations *= i
+    }
+    return combinations
+}
+function dp(node, callback) {
+    const mark = {} // 回溯标记
+    const path = []
+
+    const nLen = length(node)
+    const sum = getCombinations(nLen)
+    let isPause = false
+    let isReStart = false
+    let result = []
+    let num = 0
+    function find(pIndex = 0) {
+        if (!GLOBAL_RUN) {
+            isPause = false
+            isReStart = false
+            result = []
+            num = 0
+            return
+        }
+        const pLen = length(path)
+        let start = 0
+        if (isReStart) {
+            isPause = false
+            if (pIndex === pLen - 1) {
+                start = path[pIndex] + 1
+                path.pop()
+                isReStart = false
+            } else {
+                start = path[pIndex]
+            }
+        } else {
+            if (pLen === nLen) {
+                result.push([...path])
+                num++
+                if (num === sum) {
+                    callback(result)
+                } else {
+                    if (length(result) >= 100) {
+                        callback(result)
+                        splitTask(() => {
+                            find()
+                        })
+                        result = []
+                        isPause = true
+                        isReStart = true
+                    }
+                }
+                return
+            }
+        }
+        for (let i = start; i < nLen;i++) {
+            if (isPause || !GLOBAL_RUN) {
+                break;
+            }
+            if (!mark[i]) {
+                mark[i] = true
+                if (!isReStart) {
+                    path.push(i)
+                }
+                find(pIndex + 1)
+                mark[i] = null
+                if (!isReStart) {
+                    path.pop()
+                };
+            }
+        }
+    }
+    return {
+        run: function () {
+            start()
+            find()
+        },
+         stop: function () {
+            stop()
+         }
+    }
+}
+
+function getPrint(callback) {
+    let paths = []
+    let isPaint = false
+
+    function print(_paths, callback) {
+        let path = _paths.shift()
+        if (!path || !GLOBAL_RUN) {
+            isPaint = false
+            paths = []
+            return
+        }
+        requestAnimationFrame(() => {
+            if (isPaint) {
+                callback(path)
+                print(paths, callback)
+            } else {
+                paths = []
+            }
+        })
+    }
+    return {
+        print: () => {
+            if (!isPaint && GLOBAL_RUN) {
+                isPaint = true
+                print(paths, callback)
+            }
+        },
+        push: newPaths => {
+            if (GLOBAL_RUN) {
+                paths.push(...newPaths)
+            }
+        }
+    }
+}
+export { start, stop, getPrint, getCombinations, getDistance, dp, calc }
